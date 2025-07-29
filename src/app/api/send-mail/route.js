bb66be125d5eb7f704351e63931484f6ca612504
aswin <aswink.accolades@gmail.com>
2025-07-29 10:28:33 +0530

    conflict fix

--- src/app/api/send-mail/route.js
+++ src/app/api/send-mail/route.js
@@ -32,20 +32,19 @@ export async function POST(req) {
       </div>
     `;
 
-    await transporter.sendMail({
-      from: `"Google Ad Form" <${process.env.EMAIL_USER}>`,
-      to: ["leadsaccolades@gmail.com", "rakbangloread@gmail.com"],
-      subject: `Rak Bangalore Google Leads - ${name || "New Lead"} - ${new Date().toLocaleString()}`,
-
-      html: emailHtml,
-    });
-
-    //   await transporter.sendMail({
+    // await transporter.sendMail({
     //   from: `"Google Ad Form" <${process.env.EMAIL_USER}>`,
-    //   to: ["aswink.accolades@gmail.com"],
-    //   subject: `Rak Bangalore Google Leads - ${name || "New Lead"} - ${new Date().toLocaleString()}`,
+    //   to: ["leadsaccolades@gmail.com", "rakbangloread@gmail.com"],
+    //   subject: "Rak Bangalore Google Leads",
     //   html: emailHtml,
     // });
+
+      await transporter.sendMail({
+      from: `"Google Ad Form" <${process.env.EMAIL_USER}>`,
+      to: ["leadsaccolades@gmail.com", "rakbangloread@gmail.com"],
+      subject: "RAK Landing Page – Lead Notification",
+      html: emailHtml,
+    });
 
     return new Response(JSON.stringify({ success: true }), { status: 200 });
   } catch (error) {
